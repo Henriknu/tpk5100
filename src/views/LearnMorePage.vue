c2d00d1281196789c5bd8ca25602246532ddd1ee
Henriknu <henriknu@stud.ntnu.no>
2019-11-05 20:18:00 +0100

    Added some margin on learn more page buttons

--- src/views/LearnMorePage.vue
+++ src/views/LearnMorePage.vue
@@ -3,77 +3,23 @@
     <Nav header-text="Learn more"></Nav>
     <div class="button-div">
       <div class="button-row">
-        <button
-          @click="goToChapter(2)"
-          type="button"
-          class="btn btn-dark btn-lg"
-        >
-          Chapter 2
-        </button>
-        <button
-          @click="goToChapter(3)"
-          type="button"
-          class="btn btn-dark btn-lg"
-        >
-          Chapter 3
-        </button>
+        <button @click="goToChapter(2)" type="button" class="btn btn-dark btn-lg">Chapter 2</button>
+        <button @click="goToChapter(3)" type="button" class="btn btn-dark btn-lg">Chapter 3</button>
       </div>
       <div class="button-row">
-        <button
-          @click="goToChapter(4)"
-          type="button"
-          class="btn btn-dark btn-lg"
-        >
-          Chapter 4
-        </button>
-        <button
-          @click="goToChapter(5)"
-          type="button"
-          class="btn btn-dark btn-lg"
-        >
-          Chapter 5
-        </button>
+        <button @click="goToChapter(4)" type="button" class="btn btn-dark btn-lg">Chapter 4</button>
+        <button @click="goToChapter(5)" type="button" class="btn btn-dark btn-lg">Chapter 5</button>
       </div>
       <div class="button-row">
-        <button
-          @click="goToChapter(6)"
-          type="button"
-          class="btn btn-dark btn-lg"
-        >
-          Chapter 6
-        </button>
-        <button
-          @click="goToChapter(7)"
-          type="button"
-          class="btn btn-dark btn-lg"
-        >
-          Chapter 7
-        </button>
+        <button @click="goToChapter(6)" type="button" class="btn btn-dark btn-lg">Chapter 6</button>
+        <button @click="goToChapter(7)" type="button" class="btn btn-dark btn-lg">Chapter 7</button>
       </div>
       <div class="button-row">
-        <button
-          @click="goToChapter(8)"
-          type="button"
-          class="btn btn-dark btn-lg"
-        >
-          Chapter 8
-        </button>
-        <button
-          @click="goToChapter(9)"
-          type="button"
-          class="btn btn-dark btn-lg"
-        >
-          Chapter 9
-        </button>
+        <button @click="goToChapter(8)" type="button" class="btn btn-dark btn-lg">Chapter 8</button>
+        <button @click="goToChapter(9)" type="button" class="btn btn-dark btn-lg">Chapter 9</button>
       </div>
       <div class="button-row">
-        <button
-          @click="goToChapter(10)"
-          type="button"
-          class="btn btn-dark btn-lg"
-        >
-          Chapter 10
-        </button>
+        <button @click="goToChapter(10)" type="button" class="btn btn-dark btn-lg">Chapter 10</button>
       </div>
     </div>
   </div>
@@ -112,5 +58,11 @@ button {
 .button-row {
   display: flex;
   flex-direction: row;
+}
+
+@media only screen and (min-width: 600px) {
+  button {
+    margin: 20px;
+  }
 }
 </style>
